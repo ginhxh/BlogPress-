1f823f252d66261f55ce9845080b866b5c296a02
Ahmd Rabar <ahmedrbr010@gmail.comclcleardd>
2024-12-16 20:15:30 +0100

    fixed sign up proplemes and i added login func

--- config_seesion.inc.php
+++ config_seesion.inc.php
@@ -10,18 +10,43 @@ session_set_cookie_params([
     'httponly'=>true
 ]);
 session_start();
-if(!isset($_SESSION['last_regenertion']))
+if(isset($_SESSION['user_id'])){
+
+    if(!isset($_SESSION['last_regenertion']))
 {
-    rgenert_session_id();
+    rgenert_session_id_loggedin();
 
 }
 else{
     $interval=60*30;
     if(time()-$_SESSION['last_regenertion']>=$interval)
     {
-        rgenert_session_id();
+        rgenert_session_id_loggedin();
     }
 }
+}
+else{if(!isset($_SESSION['last_regenertion']))
+    {
+        rgenert_session_id();
+    
+    }
+    else{
+        $interval=60*30;
+        if(time()-$_SESSION['last_regenertion']>=$interval)
+        {
+            rgenert_session_id();
+        }
+    }}
+
+    function  rgenert_session_id_loggedin(){
+        session_regenerate_id(true);
+        $userID=$_SESSION['user_id'];
+        $newsession_id=session_create_id();
+        $session_id=$newsession_id. "_" .  $userID;
+
+        session_id($session_id);
+        $_SESSION['last_regenertion']=time();
+    }
 
 function rgenert_session_id(){
     session_regenerate_id(true);
--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
   require_once 'config_seesion.inc.php';
 
-  require_once 'signup_view.inc.php'
+  require_once 'signup_view.inc.php';
+  require_once 'login_view.inc.php';
 
 ?>
 
@@ -21,6 +22,7 @@
             align-items: center;
             min-height: 100vh;
             margin: 0;
+            flex-direction: column;
         }
         .form-error{color: red;}
 
@@ -83,11 +85,18 @@
 <body>
     <div class="form-container">
         <h2>Login and Sign Up</h2>
-        <form action="loginup.inc.php" method="POST">
+        <form action="login.inc.php" method="POST">
             <input type="text" name="username" placeholder="Username">
             <input type="password" name="pwd" placeholder="Password">
             <button type="submit">Log In</button>
         </form>
+
+        <?php
+
+
+check_login_errors();
+    ?>
+
         <div class="form-separator">or</div>
         <form action="signup.inc.php" method="POST">
         <?php
